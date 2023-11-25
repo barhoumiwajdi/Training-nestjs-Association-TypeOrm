06ae8d6c9e107a3840033bb37d428305011cac54
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-11-25 23:53:11 +0100

    Mail Sender

---
+++ src/Service/Email.Service.ts
@@ -0,0 +1,55 @@
+import { Injectable , HttpException ,HttpStatus} from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Injectable()
+export class ExampleService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  async example() {
+try {
+    await this.mailerService
+    .sendMail({
+      to: 'test@nestjs.com', // list of receivers
+      from: 'noreply@nestjs.com', // sender address
+      subject: 'Testing Nest MailerModule ✔', // Subject line
+      text: 'welcome', // plaintext body
+      html: '<b>welcome</b>', // HTML body content
+    })
+} catch (error) {
+    throw new HttpException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: 'This is a custom message Internal Error',
+      }, HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: error
+      });
+}
+
+ 
+  }
+
+  async example2() {
+   try {
+   await this.mailerService
+      .sendMail({
+        to: 'test@nestjs.com',
+        from: 'noreply@nestjs.com',
+        subject: 'Testing Nest Mailermodule with template ✔',
+        template: './confirmation', // The `.pug`, `.ejs` or `.hbs` extension is appended automatically.
+        context: {
+          // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe',
+        },
+      })
+    return 
+   } catch (error) {
+    throw new HttpException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: 'This is a custom message Internal Error',
+      }, HttpStatus.INTERNAL_SERVER_ERROR, {
+        cause: error
+      });
+   } 
+       
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,36 @@ import { AppService } from './app.service';
 import { PhotoModule } from './Module/Photo.Module';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './Module/User.Module';
+import { MailerModule } from '@nestjs-modules/mailer';
  
  
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
 
 @Module({
-  imports: [PhotoModule , DatabaseModule , UserModule],
+  imports: [MailerModule.forRoot({
+    transport: {
+      host: 'localhost',
+      port: 1025,
+      ignoreTLS: true,
+      secure: false,
+      auth: {
+        user: process.env.MAILDEV_INCOMING_USER,
+        pass: process.env.MAILDEV_INCOMING_PASS,
+      },
+    },
+    defaults: {
+      from: '"No Reply" <no-reply@localhost>',
+    },
+    preview: true,
+    template: {
+      dir: join(__dirname, 'templates'),
+      adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+      options: {
+        strict: true,
+      },
+    },
+  }),PhotoModule , DatabaseModule , UserModule],
   controllers: [AppController  ],
   providers: [AppService ],
 })
